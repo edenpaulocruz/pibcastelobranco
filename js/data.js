92f0028840887de48a35eb17fa18328afab42b54
edenpaulocruz <edenpaulo@gmail.com>
2024-03-08 22:50:59 -0300

    Add all questions in data file

--- js/data.js
+++ js/data.js
@@ -1,26 +1,7 @@
 export const questions = [
   {
     id: 1,
-    question: "Há quanto tempo você está em seu casamento atual?",
-    answers: {
-      a: "Menos de cinco anos.",
-      b: "Menos de dez anos.",
-      c: "Menos de quinze anos.",
-      d: "Quinze anos ou mais."
-    }
-  },
-  {
-    id: 2,
-    question: "Este é seu primeiro casamento?",
-    answers: {
-      a: "Sim.",
-      b: "Não, este é meu segundo casamento.",
-      c: "Não, este é meu terceiro casamento ou mais."
-    }
-  },
-  {
-    id: 3,
-    question: "Você e seu cônjuge atualmente oram juntos? Não considere as orações antes das refeições.",
+    question: "Você e seu cônjuge atualmente oram juntos?",
     answers: {
       a: "Todos os dias",
       b: "Quase todos os dias.",
@@ -28,16 +9,417 @@ export const questions = [
       d: "Raramente.",
       e: "Nunca."
     },
-    obs: "Se você respondeu 'Nunca', vá para a pergunta 12."
+    obs: "Se você respondeu 'Nunca', vá para a pergunta 9."
   },
   {
-    id: 4,
-    question: "Se sim, por quanto tempo?",
+    id: 2,
+    question: "Se respondeu sim a pergunta 1, por quanto tempo?",
     answers: {
-      a: "5-10 minutos.",
-      b: "15-30 minutos.",
-      c: "30-45 minutos.",
-      d: "45-60 minutos."
+      a: "1-5 minutos",
+      b: "5-10 minutos.",
+      c: "15-30 minutos.",
+      d: "30-45 minutos.",
+      e: "45-60 minutos."
     }
-  }
+  },
+  {
+		id: 3,
+		question: "Se respondeu sim a pergunta 1, vocês tem um lugar especial para orar?",
+		answers: {
+			a: "Sim.",
+			b: "Não."
+		}
+	},
+	{
+		id: 4,
+		question: "Se respondeu sim a pergunta 1, vocês tem uma hora predileta do dia para orar?",
+		answers: {
+			a: "A primeira coisa que fazemos de manhã.",
+			b: "Durante o dia.",
+			c: "Quase na hora do jantar.",
+			d: "Antes da hora de dormir."
+		}
+	},
+	{
+		id: 5,
+		question: "Se respondeu sim a pergunta 1, vocês oram em voz alta um com o outro?",
+		answers: {
+			a: "Sim.",
+			b: "Não, em silêncio."
+		}
+	},
+	{
+		id: 6,
+		question: "Se respondeu sim a pergunta 1, um de vocês geralmente fala durante a oração?",
+		answers: {
+			a: "Sim, enquanto geralmente o outro ouve.",
+			b: "Não, ambos falam."
+		}
+	},
+	{
+		id: 7,
+		question: "Se respondeu sim a pergunta 1, vocês geralmente oram juntos na hora das refeições?",
+		answers: {
+			a: "Sim.",
+			b: "Não."
+		}
+	},
+	{
+		id: 8,
+		question: "Se respondeu sim a pergunta 1, vocês oram juntos em momentos de dificuldade ou de crise?",
+		answers: {
+			a: "Sim.",
+			b: "Não."
+		}
+	},
+	{
+		id: 9,
+		question: "Vocês já pensaram em orar juntos, mas não sabem como começar?",
+		answers: {
+			a: "Sim.",
+			b: "Não, a prática de orar juntos nunca passou pela nossa cabeça."
+		}
+	},
+	{
+		id: 10,
+		question: "Na oração, vocês pedem a Deus coisas específicas que desejam que aconteça em sua vida pessoal e familiar?",
+		answers: {
+			a: "Sim.",
+			b: "Não, normalmente oramos apenas pelos outros."
+		}
+	},
+	{
+		id: 11,
+		question: "Vocês acham que tem o direito de pedir a Deus dons ou bênçãos pessoais?",
+		answers: {
+			a: "Sim.",
+			b: "Não, não fomos educados dessa forma."
+		}
+	},
+	{
+		id: 12,
+		question: "Você agradece a Deus pelas bênçãos em oração?",
+		answers: {
+			a: "Sempre.",
+			b: "Às vezes.",
+			c: "Raramente.",
+			d: "Nunca."
+		}
+	},
+	{
+		id: 13,
+		question: "Você e seu cônjuge leem a Bíblia juntos como parte de seu tempo de oração?",
+		answers: {
+			a: "Sim.",
+			b: "Não, normalmente não.",
+			c: "Sim, mas em outros momentos fora do tempo de oração."
+		}
+	},
+	{
+		id: 14,
+		question: "Levando tudo em conta, você diria que sua vida hoje é:",
+		answers: {
+			a: "Muito feliz.",
+			b: "Um pouco feliz.",
+			c: "Não muito feliz."
+		}
+	},
+	{
+		id: 15,
+		question: "Levando tudo em conta em seu casamento, você diria que ele hoje é:",
+		answers: {
+			a: "Muito feliz.",
+			b: "Um pouco feliz.",
+			c: "Não muito feliz."
+		}
+	},
+	{
+		id: 16,
+		question: "Muitas pessoas acham que o casamento está sempre passando por ciclos. Se sim, como você diria que está seu casamento no momento?",
+		answers: {
+			a: "Apaixonado.",
+			b: "Estabelecendo-se.",
+			c: "Afundando.",
+			d: "Recomeçando."
+		}
+	},
+	{
+		id: 17,
+		question: "Considerando que a maioria dos casais às vezes discute, quantas vezes você diria que as palavras atravessadas são ditas em uma semana, em média?",
+		answers: {
+			a: "Todos os dias.",
+			b: "De três a cinco vezes por semana.",
+			c: "Algumas vezes por mês.",
+			d: "Raramente.",
+			e: "Nunca."
+		}
+	},
+	{
+		id: 18,
+		question: "Você acha que você e seu cônjuge podem conversar um com o outro com a frequência que você gostaria?",
+		answers: {
+			a: "Sim.",
+			b: "Não, precisamos ter mais tempo para conversar."
+		}
+	},
+	{
+		id: 19,
+		question: "Em média, quantas conversas vocês imaginam que têm um com o outro em um dia?",
+		answers: {
+			a: "Uma ou duas vezes.",
+			b: "Várias vezes.",
+			c: "Muitas.",
+		}
+	},
+	{
+		id: 20,
+		question: "Qual seria uma estimativa aproximada do total de minutos que você conversa com seu cônjuge durante um dia, em média?",
+		answers: {
+			a: "5-10 minutos.",
+			b: "15-30 minutos.",
+			c: "60 minutos ou mais.",
+		}
+	},
+	{
+		id: 21,
+		question: "Quantas vezes por mês você e seu cônjuge fazem amor?",
+		answers: {
+			a: "Raramente, ou nunca.",
+			b: "Uma vez ou menos.",
+			c: "Geralmente menos de quatro vezes.",
+			d: "Provavelmente uma média de cinco a dez vezes.",
+			e: "Mais?"
+		}
+	},
+	{
+		id: 22,
+		question: "Qual das seguintes opções melhor descreve seus sentimentos com relação à frequência de suas relações sexuais?",
+		answers: {
+			a: "Eu gostaria que elas fossem mais frequentes.",
+			b: "Está ótimo agora.",
+			c: "Não estou interessado."
+		}
+	},
+	{
+		id: 23,
+		question: "Qual das opções descreve o nível de satisfação que você sente no ato sexual?",
+		answers: {
+			a: "Eu gostaria que fosse mais satisfatório.",
+			b: "Está ótimo agora."
+		}
+	},
+	{
+		id: 24,
+		question: "Você concorda que existe uma correlação entre o ato sexual e a oração?",
+		answers: {
+			a: "Eu duvido.",
+			b: "É possível, mas não tenho certeza.",
+			c: "Concordo um pouco.",
+			d: "Concordo plenamente."
+		}
+	},
+	{
+		id: 25,
+		question: "Em uma escala de 1 a 10, como você avaliaria sua felicidade conjugal no momento?",
+		answers: {
+			a: "1",
+			b: "2",
+			c: "3",
+			d: "4",
+			e: "5",
+			f: "6",
+			g: "7",
+			h: "8",
+			i: "9",
+			j: "10"
+		}
+	},
+	{
+		id: 26,
+		question: "Quantas vezes você e seu cônjuge dão-se as mãos?",
+		answers: {
+			a: "Muitas.",
+			b: "De vez em quando.",
+			c: "Raramente.",
+			d: "Nunca."
+		}
+	},
+	{
+		id: 27,
+		question: "Quantas vezes você e seu cônjuge riem juntos?",
+		answers: {
+			a: "Muitas.",
+			b: "De vez em quando.",
+			c: "Raramente.",
+			d: "Nunca."
+		}
+	},
+	{
+		id: 28,
+		question: "Quantas vezes você e seu cônjuge saem para passear, ir ao cinema, andar de bicicleta, caminhar ou fazer outras atividades?",
+		answers: {
+			a: "Muitas.",
+			b: "De vez em quando.",
+			c: "Raramente.",
+			d: "Nunca."
+		}
+	},
+	{
+		id: 29,
+		question: "Quantas vezes você e seu cônjuge tocam-se de forma carinhosa?",
+		answers: {
+			a: "Muitas.",
+			b: "De vez em quando.",
+			c: "Raramente.",
+			d: "Nunca."
+		}
+	},
+	{
+		id: 30,
+		question: "Quantas vezes você e seu cônjuge passam a mão nas costas ou nos pés um do outro?",
+		answers: {
+			a: "Muitas.",
+			b: "De vez em quando.",
+			c: "Raramente.",
+			d: "Nunca."
+		}
+	},
+	{
+		id: 31,
+		question: "Quantas vezes você e seu cônjuge ficam juntos para namorar?",
+		answers: {
+			a: "Muitas.",
+			b: "De vez em quando.",
+			c: "Raramente.",
+			d: "Nunca."
+		}
+	},
+	{
+		id: 32,
+		question: "Quantas vezes você e seu cônjuge deixam bilhetes de amor um para o outro?",
+		answers: {
+			a: "Muitas.",
+			b: "De vez em quando.",
+			c: "Raramente.",
+			d: "Nunca."
+		}
+	},
+	{
+		id: 33,
+		question: "Quantas vezes você e seu cônjuge vão juntos a festas?",
+		answers: {
+			a: "Muitas.",
+			b: "De vez em quando.",
+			c: "Raramente.",
+			d: "Nunca."
+		}
+	},
+	{
+		id: 34,
+		question: "Quantas vezes você e seu cônjuge gostam de ouvir as mesmas músicas e assistir filmes juntos?",
+		answers: {
+			a: "Muitas.",
+			b: "De vez em quando.",
+			c: "Raramente.",
+			d: "Nunca."
+		}
+	},
+	{
+		id: 35,
+		question: "Quantas vezes você e seu cônjuge conversam em particular e de forma amorosa um com o outro?",
+		answers: {
+			a: "Muitas.",
+			b: "De vez em quando.",
+			c: "Raramente.",
+			d: "Nunca."
+		}
+	},
+	{
+		id: 36,
+		question: "Quantas vezes você e seu cônjuge perguntam sobre o trabalho e as atividades um do outro?",
+		answers: {
+			a: "Muitas.",
+			b: "De vez em quando.",
+			c: "Raramente.",
+			d: "Nunca."
+		}
+	},
+	{
+		id: 37,
+		question: "Quantas vezes você e seu cônjuge vão juntos a cerimônias religiosas?",
+		answers: {
+			a: "Muitas.",
+			b: "De vez em quando.",
+			c: "Raramente.",
+			d: "Nunca."
+		}
+	},
+	{
+		id: 38,
+		question: "Quantas vezes você e seu cônjuge honram um ao outro de modo respeitoso?",
+		answers: {
+			a: "Muitas.",
+			b: "De vez em quando.",
+			c: "Raramente.",
+			d: "Nunca."
+		}
+	},
+	{
+		id: 39,
+		question: "Quantas vezes você e seu cônjuge realmente tentam entender os sentimentos um do outro?",
+		answers: {
+			a: "Muitas.",
+			b: "De vez em quando.",
+			c: "Raramente.",
+			d: "Nunca."
+		}
+	},
+	{
+		id: 40,
+		question: "Quantas vezes você e seu cônjuge protegem o casamento cuidando um do outro?",
+		answers: {
+			a: "Muitas.",
+			b: "De vez em quando.",
+			c: "Raramente.",
+			d: "Nunca."
+		}
+	},
+	{
+		id: 41,
+		question: "Considerando onde seu casamento está neste momento, como você classificaria o medo do divórcio?",
+		answers: {
+			a: "50/50.",
+			b: "25% ou algo em torno disso.",
+			c: "10% ou algo em torno disso.",
+			d: "1% ou algo em torno disso."
+		}
+	},
+	{
+		id: 42,
+		question: "Alguns maridos/esposas gostam de sair com amigos à noite. Isso é algo que atrai você?",
+		answers: {
+			a: "Muito.",
+			b: "Às vezes, é divertido.",
+			c: "Raramente, eu prefiro estar com minha esposa/marido."
+		}
+	},
+	{
+		id: 43,
+		question: "Qual das respostas melhor descreve o modo como você considera se casamento?",
+		answers: {
+			a: "Um casal fazendo o melhor possível para mantê-lo unido.",
+			b: "Companhias muito boas na maior parte do tempo.",
+			c: "Melhor amigo um do outro com uma base sólida."
+		}
+	},
+	{
+		id: 44,
+		question: "Qual das opções melhor descreve sua experiência com sua igreja ou organização religiosa?",
+		answers: {
+			a: "Recebemos pouco ou nenhum incentivo para orarmos um com o outro.",
+			b: "Ouvimos pouco sobre a prática de orar com o cônjuge.",
+			c: "Ouvimos certo incentivo sobre este assunto.",
+			d: "Há uma forte ênfase e incentivo para que os cônjuges orem um com o outro."
+		}
+	}
 ];
